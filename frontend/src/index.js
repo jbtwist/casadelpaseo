c9af89fbb32fa8ac12411351a6b4351c6e2ab715
Juan Bautista Villarejo Ortiz <jbtwist92@gmail.com>
2024-12-18 21:45:23 +0000

    Fix Restaurant gap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('react-root'))
 
 export const styles = {
   title: 'text-2xl lg:text-3xl md:text-4xl font-bold text-[#478942] border-b-2 border-[#478942]/60 inline-block pb-2',
-  titleDesc: 'mt-6 text-gray-700 text-lg lg:text-base leading-relaxed text-left',
+  titleDesc: 'my-6 text-gray-700 text-lg lg:text-base leading-relaxed text-left',
   activity: {
     image: 'h-64 w-full md:h-80 lg:h-96 xl:h-[30rem] 2xl:h-[35rem]',
     container: 'flex lg:flex-row flex-col max-w-6xl mx-auto my-5',
